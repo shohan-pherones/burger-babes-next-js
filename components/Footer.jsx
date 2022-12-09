b7dead0ec41050d2a897ff2d93b9f47a018b5186
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-09 14:15:53 +0600

    footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,27 @@
+import {
+  UilFacebook,
+  UilInstagram,
+  UilTwitter,
+} from "@iconscout/react-unicons";
+import css from "../styles/Footer.module.css";
+import Logo from "../assets/Logo.png";
+import Image from "next/image";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={css.wrapper}>
+      <span>
+        &copy; {new Date().getFullYear()} Burger Babes. All rights reserved.
+      </span>
+      <div className={css.socials}>
+        <UilFacebook size={35} />
+        <UilInstagram size={35} />
+        <UilTwitter size={35} />
+      </div>
+      <div className={css.logo}>
+        <Image src={Logo} alt="logo" width={50} height={50} />
+        <span>Burger Babes</span>
+      </div>
+    </div>
+  );
 }
